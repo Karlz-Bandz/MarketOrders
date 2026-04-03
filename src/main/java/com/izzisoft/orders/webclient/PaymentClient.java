acd1660d374c93c655f86f479ff5709eb5f23a73
Karlz-Bandz <karol2646@gmail.com>
2026-04-03 23:15:51 +0200

    Remove WebFLux

--- src/main/java/com/izzisoft/orders/webclient/PaymentClient.java
+++ src/main/java/com/izzisoft/orders/webclient/PaymentClient.java
@@ -4,20 +4,24 @@ import com.izzisoft.orders.dto.PaymentRequest;
 import com.izzisoft.orders.dto.PaymentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @RequiredArgsConstructor
 public class PaymentClient {
 
-    private final WebClient paymentWebClient;
+    private final RestTemplate restTemplate;
+
+    private static final String PAYMENT_SERVICE_URL = "http://localhost:8083";
 
     public PaymentResponse processPayment(PaymentRequest paymentRequest) {
-        return paymentWebClient.post()
-                .uri("/payment")
-                .bodyValue(paymentRequest)
-                .retrieve()
-                .bodyToMono(PaymentResponse.class)
-                .block();
+
+        String url = PAYMENT_SERVICE_URL + "/payment";
+
+        return restTemplate.postForObject(
+                url,
+                paymentRequest,
+                PaymentResponse.class
+        );
     }
 }
--- src/main/java/com/izzisoft/orders/webclient/ProductClient.java
+++ src/main/java/com/izzisoft/orders/webclient/ProductClient.java
@@ -3,39 +3,65 @@ package com.izzisoft.orders.webclient;
 import com.izzisoft.orders.dto.ProductResponse;
 import com.izzisoft.orders.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
 
 @Component
 @RequiredArgsConstructor
 public class ProductClient {
 
-    private final WebClient productWebClient;
+    private static final String PRODUCT_SERVICE_URL = "http://localhost:8080";
+
+    private final RestTemplate restTemplate;
 
     private final JwtService jwtService;
 
     public void increaseProductQuantity(Long productId, int increaseValue) {
+
         String serviceToken = jwtService.generateServiceToken();
 
-        productWebClient.put()
-                .uri("/products/increase/{id}/{increaseValue}", productId, increaseValue)
-                .header("Authorization", "Bearer " + serviceToken)
-                .retrieve()
-                .toBodilessEntity()
-                .block();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + serviceToken);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        String url = PRODUCT_SERVICE_URL + "/products/increase/{id}/{increaseValue}";
+
+        restTemplate.exchange(
+                url,
+                HttpMethod.PUT,
+                entity,
+                Void.class,
+                productId,
+                increaseValue
+        );
     }
 
     public void decreaseProductQuantity(Long productId, int decreaseValue) {
-      String serviceToken = jwtService.generateServiceToken();
 
-      productWebClient.put()
-               .uri("/products/decrease/{id}/{decreaseValue}", productId, decreaseValue)
-               .header("Authorization", "Bearer " + serviceToken)
-               .retrieve()
-               .toBodilessEntity()
-               .block();
+        String serviceToken = jwtService.generateServiceToken();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + serviceToken);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        String url = PRODUCT_SERVICE_URL + "/products/decrease/{id}/{decreaseValue}";
+
+        restTemplate.exchange(
+                url,
+                HttpMethod.PUT,
+                entity,
+                Void.class,
+                productId,
+                decreaseValue
+        );
     }
 
     public ProductResponse getProductById(Long productId) {
@@ -43,11 +69,22 @@ public class ProductClient {
 
         String userToken = auth.getToken().getTokenValue();
 
-        return productWebClient.get()
-                .uri("/products/{id}", productId)
-                .header("Authorization", "Bearer " + userToken)
-                .retrieve()
-                .bodyToMono(ProductResponse.class)
-                .block();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + userToken);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        String url = PRODUCT_SERVICE_URL + "/products/{id}";
+
+        ResponseEntity<ProductResponse> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                ProductResponse.class,
+                productId
+
+        );
+
+        return response.getBody();
     }
 }
